de07b7b2ca46ae7801bd2f2b2d78dd47085f0f3a
dariusz-todoit <tarakena@gmail.com>
2018-02-14 07:56:12 +0100

    version with ListBox improved

--- NotesManager/src/com/todoit/training/client/NotesManager.java
+++ NotesManager/src/com/todoit/training/client/NotesManager.java
@@ -24,102 +24,7 @@ public class NotesManager implements EntryPoint {
   private ArrayList<Project> projectList = new ArrayList<Project> ();
   
   private void showPage () {
-    RootPanel.get("gwtContainer").clear();    
-    Button newNoteButton = new Button("New note");
-    final PopupPanel newNotePopup = new PopupPanel();
-    final TextArea textArea1 = new TextArea();
-    textArea1.setCharacterWidth(20);
-    textArea1.setVisibleLines(5);
     
-    final VerticalPanel vPanel = new VerticalPanel();
-    vPanel.add(textArea1);
-    
-    final ListBox listBox = new ListBox ();
-    for (int i = 0; i < projectList.size (); i++) {
-      listBox.addItem (projectList.get (i).getName ());
-    }
-    listBox.setVisibleItemCount (1);
-    vPanel.add (listBox);
-    
-    Button saveNewNoteButton = new Button("Save note");
-    saveNewNoteButton.setWidth("100px");
-    vPanel.add(saveNewNoteButton);
-    newNotePopup.setWidget(vPanel);
-    newNoteButton.addClickHandler(new ClickHandler() {
-      public void onClick(ClickEvent event) {    
-        newNotePopup.center();
-      }
-    }); // newNoteButton.addClickHandler(new ClickHandler()
-    
-    saveNewNoteButton.addClickHandler(new ClickHandler() {
-      @Override
-      public void onClick(ClickEvent event) {
-        newMessage (textArea1.getText(), listBox.getSelectedIndex());
-        newNotePopup.hide();
-      } // public void onClick(ClickEvent event)
-    }); // saveNewNoteButton.addClickHandler(new ClickHandler()
-    
-    
-    FlexTable flexTable = new FlexTable();
-    VerticalPanel verticalPanel = new VerticalPanel();
-    verticalPanel.add(newNoteButton);
-    verticalPanel.add(flexTable);
-    
-    for (int i = 0; i < messageList.size(); i++) {
-      final int j = i;
-      Message msg = messageList.get(i);
-      Button removeButton = new Button ("Remove note " + msg.getMessageID());
-      Button updateButton = new Button ("Update note " + msg.getMessageID());
-      flexTable.setHTML(i, 0, "" + msg.getMessageID());
-      flexTable.setHTML(i, 1, msg.getNote());
-      flexTable.setHTML(i, 2, msg.getProjectName());
-      flexTable.setWidget(i, 3, removeButton);
-      flexTable.setWidget(i, 4, updateButton);
-    
-      removeButton.addClickHandler (new ClickHandler() {        
-        @Override
-        public void onClick(ClickEvent event) {
-          remMessage (j);
-        } // public void onClick(ClickEvent event)
-      }); // removeButton[i].addClickHandler (new ClickHandler()
-    
-     
-      updateButton.addClickHandler (new ClickHandler() {
-        @Override
-        public void onClick (ClickEvent event) {
-          final DialogBox updateDialog = new DialogBox();
-          final Button saveButton = new Button ("Save note " + messageList.get(j).getMessageID());          
-          VerticalPanel vp = new VerticalPanel();
-          final TextArea textArea2 = new TextArea();
-          textArea2.setText (messageList.get (j).getNote ());
-          vp.add(textArea2);
-          vp.add(saveButton);
-          
-          updateDialog.add(vp);
-                      
-          int left = Window.getClientWidth()/ 2;
-          int top = Window.getClientHeight()/ 2;
-          updateDialog.setPopupPosition(left, top);
-          updateDialog.show();
-          
-          saveButton.addClickHandler (new ClickHandler() {
-            @Override
-            public void onClick (ClickEvent event) {
-              final Message message = new Message (messageList.get(j).getMessageID(), textArea2.getText(), 
-                messageList.get(j).getProjectID(), messageList.get(j).getProjectName());              
-              updMessage (j, message);
-              updateDialog.hide();
-            } // public void onClick (ClickEvent event)
-          }); // saveButton.addClickHandler (new ClickHandler()
-        } // public void onClick (ClickEvent event)
-      }); // updateButton[i].addClickHandler (new ClickHandler()
-      
-    } // for (int i = 0; i < messageList.size(); i++)
-    
-    RootPanel.get("gwtContainer").add(verticalPanel);    
-  } // private void showPage ()
-
-  public void onModuleLoad() {
     messageService.getMessages (new AsyncCallback<ArrayList<Message>> () {
       @Override
       public void onFailure (Throwable caught) {
@@ -130,34 +35,134 @@ public class NotesManager implements EntryPoint {
       public void onSuccess (ArrayList<Message> result) {
         messageList = result;
         
-        messageService.getProjects (new AsyncCallback<ArrayList<Project>> () {
+        RootPanel.get("gwtContainer").clear();
+        Button newNoteButton = new Button("New note");
+        final PopupPanel newNotePopup = new PopupPanel();
+        final TextArea newNoteTextArea = new TextArea();
+        newNoteTextArea.setCharacterWidth(20);
+        newNoteTextArea.setVisibleLines(5);
+    
+        final VerticalPanel vPanel = new VerticalPanel();
+        vPanel.add(newNoteTextArea);
+    
+        final ListBox projectsListBox = new ListBox ();
+        projectsListBox.addItem ("","");
+        for (Project project: projectList) {
+          projectsListBox.addItem (project.getName (), Integer.toString (project.getID ()));
+        }
+        projectsListBox.setVisibleItemCount (1);
+        vPanel.add (projectsListBox);
+    
+        Button saveNewNoteButton = new Button("Save note");
+        saveNewNoteButton.setWidth("100px");
+        vPanel.add(saveNewNoteButton);
+        newNotePopup.setWidget(vPanel);
+        newNoteButton.addClickHandler(new ClickHandler() {
+          public void onClick(ClickEvent event) {    
+            newNotePopup.center();
+          }
+        }); // newNoteButton.addClickHandler(new ClickHandler()
+    
+        saveNewNoteButton.addClickHandler(new ClickHandler() {
           @Override
-          public void onFailure (Throwable caught) {
-            /* server side error occurred */
-            onFailureAlert (caught.getMessage());
-          } // public void onFailure (Throwable caught)
-          @Override
-          public void onSuccess (ArrayList<Project> result2) {
-            projectList = result2;
-            showPage ();               
-          } // public void onSuccess (ArrayList<Message> result)
+          public void onClick(ClickEvent event) {
+            String value = projectsListBox.getValue (projectsListBox.getSelectedIndex());
+            if (value.equals ("")) {
+              Window.alert ("Wybierz projekt!");
+            } else {
+              int projectID = Integer.parseInt (value);
+              newMessage (newNoteTextArea.getText(), projectID);
+              newNotePopup.hide();
+            }        
+          } // public void onClick(ClickEvent event)
+        }); // saveNewNoteButton.addClickHandler(new ClickHandler()
+    
+    
+        FlexTable flexTable = new FlexTable();
+        VerticalPanel verticalPanel = new VerticalPanel();
+        verticalPanel.add(newNoteButton);
+        verticalPanel.add(flexTable);
+    
+        for (int i = 0; i < messageList.size(); i++) {
+          final int j = i;
+          Message msg = messageList.get(i);
+          Button removeButton = new Button ("Remove note " + msg.getMessageID());
+          Button updateButton = new Button ("Update note " + msg.getMessageID());
+          flexTable.setHTML(i, 0, "" + msg.getMessageID());
+          flexTable.setHTML(i, 1, msg.getNote());
+          flexTable.setHTML(i, 2, msg.getProjectName());
+          flexTable.setWidget(i, 3, removeButton);
+          flexTable.setWidget(i, 4, updateButton);
+    
+          removeButton.addClickHandler (new ClickHandler() {        
+            @Override
+            public void onClick(ClickEvent event) {
+              remMessage (j);
+            } // public void onClick(ClickEvent event)
+          }); // removeButton[i].addClickHandler (new ClickHandler()
+    
+     
+          updateButton.addClickHandler (new ClickHandler() {
+            @Override
+            public void onClick (ClickEvent event) {
+              final DialogBox updateDialog = new DialogBox();
+              final Button saveButton = new Button ("Save note " + messageList.get(j).getMessageID());          
+              VerticalPanel vp = new VerticalPanel();
+              final TextArea updateNoteTextArea = new TextArea();
+              updateNoteTextArea.setText (messageList.get (j).getNote ());
+              vp.add(updateNoteTextArea);
+              vp.add(saveButton);
           
-        });        
-        
-        // showPage ();               
-      } // public void onSuccess (ArrayList<Message> result)
+              updateDialog.add(vp);
+                      
+              int left = Window.getClientWidth()/ 2;
+              int top = Window.getClientHeight()/ 2;
+              updateDialog.setPopupPosition(left, top);
+              updateDialog.show();
+          
+              saveButton.addClickHandler (new ClickHandler() {
+                @Override
+                public void onClick (ClickEvent event) {
+                  final Message message = new Message (messageList.get(j).getMessageID(), updateNoteTextArea.getText(), 
+                    messageList.get(j).getProjectID(), messageList.get(j).getProjectName());              
+                  updMessage (j, message);
+                  updateDialog.hide();
+                } // public void onClick (ClickEvent event)
+              }); // saveButton.addClickHandler (new ClickHandler()
+            } // public void onClick (ClickEvent event)
+          }); // updateButton[i].addClickHandler (new ClickHandler()
       
-    });// messageService.getMessages (new AsyncCallback<ArrayList<Message>> ()
+    } // for (int i = 0; i < messageList.size(); i++)
+    
+    RootPanel.get("gwtContainer").add(verticalPanel);
+  
+    }});
+      
+  } // private void showPage ()
+      
+   
+
+  public void onModuleLoad() {
+    messageService.getProjects (new AsyncCallback<ArrayList<Project>> () {
+      @Override
+      public void onFailure (Throwable caught) {
+        /* server side error occurred */
+        onFailureAlert (caught.getMessage());
+      } // public void onFailure (Throwable caught)
+      @Override
+      public void onSuccess (ArrayList<Project> result) {
+        projectList = result;
+        showPage ();
+      }           
+    });    
   } // public void onModuleLoad() 
 
   private void onFailureAlert (String msg) {
     Window.alert ("Unable to obtain server response: " + msg);
   }
   
-  private void newMessage (String newNote, int selectedValue) {
-    final String newNote1 = newNote;
-    final int projectID = projectList.get(selectedValue).getID();
-    final String projectName = projectList.get(selectedValue).getName();
+  
+  private void newMessage (String newNote, int projectID) {
     messageService.createNewMessage (newNote, projectID, new AsyncCallback<Integer> () {  
       @Override
       public void onFailure (Throwable caught) {
@@ -167,15 +172,12 @@ public class NotesManager implements EntryPoint {
 
       @Override
       public void onSuccess (Integer newID) {
-        Message message = new Message (newID, newNote1, projectID, projectName);           
-        messageList.add (message);                
         showPage ();               
       } // public void onSuccess (String newID)   
-    }); // messageService.createNewMessage (textArea1.getText(), new AsyncCallback<String> ()
+    }); // messageService.createNewMessage (newNoteTextArea.getText(), new AsyncCallback<String> ()
   }
   
   private void remMessage (int index) {
-    final int index1 = index;
     messageService.removeMessage (messageList.get(index).getMessageID(), new AsyncCallback<Boolean> () {
       @Override
       public void onFailure(Throwable caught) {
@@ -184,15 +186,12 @@ public class NotesManager implements EntryPoint {
       } // public void onFailure(Throwable caught)
       @Override
       public void onSuccess (Boolean result) {
-        messageList.remove(index1);
         showPage ();              
       } // public void onSuccess (Boolean result)    
     }); // messageService.removeMessage (messageList.get(j).getMessageID(), new AsyncCallback<Boolean> ()
   }
   
   private void updMessage (int index, Message message) {
-    final int index1 = index;
-    final Message message1 = message;
     messageService.updateMessage (message, new AsyncCallback<Boolean> () {
       @Override
       public void onFailure (Throwable caught) {
@@ -201,8 +200,6 @@ public class NotesManager implements EntryPoint {
       } // public void onFailure (Throwable caught)
       @Override
       public void onSuccess (Boolean result) {
-        messageList.remove (index1);
-        messageList.add (index1, message1);
         showPage ();      
       } // public void onSuccess (Boolean result)
     }); // messageService.updateMessage (message, new AsyncCallback<Boolean> ()
